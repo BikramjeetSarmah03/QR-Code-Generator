ec3401ea2d1e312938b257ce1ea1f00b49de16af
Bikramjeet Sarmah <9854bikram@gmail.com>
2022-01-25 10:18:14 +0530

    First Commit QR-CODE Generator

---
+++ script.js
@@ -0,0 +1,25 @@
+let qrCode = document.querySelector("img");
+let qrText = document.getElementById('input');
+let download = document.getElementById('download');
+
+// qrBtn.addEventListener("click", generateQR);
+
+function generateQR(){
+     let data = qrText.value;
+     if(data.length == 0){
+          qrCode.src = "QR-Code Preview.svg";
+     }else{
+          let size = "1000x1000";
+          let baseURL = "http://api.qrserver.com/v1/create-qr-code/";
+
+          let url = `${baseURL}?data=${data}&size=${size}`;
+
+          qrCode.src = url;
+     }
+}
+
+function qrDownload(){
+     let imgPath = qrCode.getAttribute('src');
+     
+     saveAs(imgPath, "QR-Code");
+}
